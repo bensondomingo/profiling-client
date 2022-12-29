8ae7c342b8e4879e0cdeac7fd0556e8837ce60f7
Benson Domingo <benson.domingo@outlook.com>
2022-12-29 21:36:29 +0800

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import { Box } from '@mui/material'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { Profiles } from './components/Profiles'
 import TopBar from './components/layout/TopBar'
 import SideBar from './components/layout/SideBar'
 
 const queryClient = new QueryClient()
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Profiles />,
+  },
+])
 
 const App = () => {
   const [mobileOpen, setMobileOpen] = useState(false)
@@ -18,8 +25,11 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Box sx={{ display: 'flex' }}>
-        <TopBar onToggleSideNav={handleDrawerToggle}></TopBar>
-        <SideBar mobileOpen={mobileOpen} onClose={handleDrawerToggle}></SideBar>
+        <TopBar onToggleSideNav={handleDrawerToggle} />
+        <SideBar mobileOpen={mobileOpen} onClose={handleDrawerToggle} />
+        <div style={{ marginTop: '100px', width: '100%' }}>
+          <RouterProvider router={router} />
+        </div>
       </Box>
     </QueryClientProvider>
   )
