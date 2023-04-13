3fd87bc0252db87e59e50aedb42912e56ad1daae
Benson Domingo <benson.domingo@outlook.com>
2023-04-14 00:28:46 +0800

    Implement basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import { useState } from 'react'
 import { Box } from '@mui/material'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
 import { ProfileList } from './components/ProfileList'
 import TopBar from './components/layout/TopBar'
 import SideBar from './components/layout/SideBar'
 
 const queryClient = new QueryClient()
+
+const AppLayout = () => {
+  const [mobileOpen, setMobileOpen] = useState(false)
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  return (
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <TopBar onToggleSideNav={handleDrawerToggle} />
+        <SideBar mobileOpen={mobileOpen} onClose={handleDrawerToggle} />
+        <div
+          style={{ marginTop: '100px', marginBottom: '200px', width: '100%' }}
+        >
+          <Outlet />
+        </div>
+      </Box>
+    </>
+  )
+}
+
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <ProfileList />,
+    path: '',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '',
+        element: <ProfileList />,
+      },
+    ],
   },
 ])
 
@@ -24,15 +52,7 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Box sx={{ display: 'flex' }}>
-        <TopBar onToggleSideNav={handleDrawerToggle} />
-        <SideBar mobileOpen={mobileOpen} onClose={handleDrawerToggle} />
-        <div
-          style={{ marginTop: '100px', marginBottom: '200px', width: '100%' }}
-        >
-          <RouterProvider router={router} />
-        </div>
-      </Box>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 }
