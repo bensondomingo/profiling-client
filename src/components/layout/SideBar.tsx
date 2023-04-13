dd27c84927d7a4aab5eb0bb71250f96f7ffb0fbe
Benson Domingo <benson.domingo@outlook.com>
2023-04-14 00:10:59 +0800

    Improve SideBar component

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
-import CssBaseline from '@mui/material/CssBaseline'
 import Divider from '@mui/material/Divider'
 import Drawer from '@mui/material/Drawer'
-import IconButton from '@mui/material/IconButton'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
+import PeopleIcon from '@mui/icons-material/People'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import MailIcon from '@mui/icons-material/Mail'
-import MenuIcon from '@mui/icons-material/Menu'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import { Link, useLocation } from 'react-router-dom'
 
 const drawerWidth = 240
 
@@ -25,32 +21,31 @@ interface Props {
 
 export default function ResponsiveDrawer(props: Props) {
   const { mobileOpen, onClose } = props
+  const { pathname } = useLocation()
+
+  const paths = [
+    {
+      route: '/',
+      name: 'Profiles',
+    },
+    {
+      route: '/add',
+      name: 'Add Profile',
+    },
+  ]
 
   const drawer = (
     <div>
       <Toolbar />
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
+        {paths.map((path, index) => (
+          <ListItem disablePadding key={index} component={Link} to={path.route}>
+            <ListItemButton selected={pathname === path.route}>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <PeopleIcon />
               </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText>{path.name}</ListItemText>
             </ListItemButton>
           </ListItem>
         ))}
